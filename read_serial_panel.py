c53c60164e0cb91f648dac40d016b0424d23df6d
JensBorg <borg_jens@hotmail.com>
2020-05-28 23:36:02 +0200

    Added service file and cleaned a little up in the code

--- read_serial_panel.py
+++ read_serial_panel.py
@@ -18,20 +18,13 @@ Tx_Enable = LED(18)
 #:ReadQuery2 $END;
 #E $Data(BYTE[1]) $CheckSum16(BYTE[2]) $END;
 
-
-#Inverter 1: b'7f' b'df' b'eb' b'ff' b'df' b'5d' b'77' b'00' b'' b''
-#Inverter 2: b'bf' b'df' b'eb' b'ff' b'df' b'5d' b'11' b'00' b'' b''
-
 # 9600, 8 bit, 1 stop bit (checksum passer med MODBUS checksum)
 #2020-05-22 22:53:17.435546: b'01'b'04'b'00'b'0a'b'00'b'10'b'd1'b'c4'.
-#values1 = bytearray ([0x01, 0x04, 0x00, 0x0a, 0x00, 0x10, 0xd1, 0xc4])
 values1 = (b'\x01\x04\x00\x0a\x00\x10\xd1\xc4')
-
 
 values11 = bytearray ([0x01, 0x04, 0x00, 0x1e, 0x00, 0x0a, 0x10, 0x0b])
 
 #2020-05-22 22:53:13.385091: b'02'b'04'b'00'b'0a'b'00'b'10'b'd1'b'f7'.
-#values2 = bytearray ([0x02, 0x04, 0x00, 0x0a, 0x00, 0x10, 0xd1, 0xf7])
 values2 = (b'\x02\x04\x00\x0a\x00\x10\xd1\xf7')
 values21 = bytearray ([0x02, 0x04, 0x00, 0x1e, 0x00, 0x0a, 0x10, 0x38])
 
@@ -112,13 +105,11 @@ with serial.rs485.RS485('/dev/ttyAMA0', 9600, bytesize=8, parity='N', stopbits=1
                 print(datetime.datetime.now(), " ",end='')
 #                print("0+1: ", msg[0], " - ", msg[1], " - ", (msg[0]*256+msg[1])/10, "")
 #                print("2+3: ", msg[2], " - ", msg[3], " - ", (msg[2]*256+msg[3])/10, "")
-#                print("3+4: ", msg[3], " - ", msg[4], " - ", (msg[3]*256+msg[4])/10, "PV Volt")
                 txt=txt + "PV Volt " + format(to_value(msg,3,2,10)) + ", "
                 print("PV Volt",to_value(msg,3,2,10)," ",end='')
 #                print("5+6: ", msg[5], " - ", msg[6], " - ", msg[5]*256+msg[6])
 #                print("7+8: ", msg[7], " - ", msg[8], " - ", msg[7]*256+msg[8])
 #                print("9+10: ", msg[9], " - ", msg[10], " - ", msg[9]*256+msg[10])
-#                print("11+12: ", msg[11], " - ", msg[12], " - ", (msg[11]*256+msg[12])/10, "Volt")
                 txt=txt + "Volt " + format(to_value(msg,11,2,10)) + ", "
                 print("Volt",to_value(msg,11,2,10)," ",end='')
 #                print("13+14: ", msg[13], " - ", msg[14], " - ", msg[13]*256+msg[14])
@@ -126,17 +117,13 @@ with serial.rs485.RS485('/dev/ttyAMA0', 9600, bytesize=8, parity='N', stopbits=1
 #                print("17+18: ", msg[17], " - ", msg[18], " - ", msg[17]*256+msg[18])
 #                print("19+20: ", msg[19], " - ", msg[20], " - ", msg[19]*256+msg[20])
 #                print("21+22: ", msg[21], " - ", msg[22], " - ", msg[21]*256+msg[22])
-#                print("23+24: ", msg[23], " - ", msg[24], " - ", (msg[23]*256+msg[24])/100, " Hz")
                 txt=txt + "Hz " + format(to_value(msg,23,2,100)) + ", "
                 print("Hz",to_value(msg,23,2,100)," ",end='')
 #                print("25+26: ", msg[25], " - ", msg[26], " - ", msg[25]*256+msg[26])
-#                print("27+28: ", msg[27], " - ", msg[28], " - ", (msg[27]*256+msg[28])/10, " Watt")
                 txt=txt + "Watt " + format(to_value(msg,27,2,10)) + ", "
                 print("Watt",to_value(msg,27,2,10)," ",end='')
-#                print("29+30: ", msg[29], " - ", msg[30], " - ", (msg[29]*256+msg[30])/10, " kWatt today")
                 txt=txt + "KWatt today " + format(to_value(msg,29,2,10)) + ", "
                 print("KWatt today",to_value(msg,29,2,10)," ",end='')
-#                print("31+32+33+43: ", msg[31], " - ", msg[32], " - " , msg[33], " - ", msg[34], " - ", (msg[31]*256*65536+msg[32]*65536+ msg[33]*256+msg[34])/10, " kWatt total ")
                 txt=txt + "KWatt total " + format(to_value(msg,31,4,10))
                 print("Kwatt total",to_value(msg,31,4,10))
                 txt=txt + "\n"
